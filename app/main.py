9e0ae729345e15c400fb5f80ef66091408298b1d
DrawEnglihs <drawenglish@gmail.com>
2025-05-01 23:26:43 +0900

    up

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 #아래 2줄은 FastAPI에서 /openapi.json 엔드포인트 추가하기 위한 부분
-#from fastapi.responses import FileResponse
-#import os
+from fastapi.responses import FileResponse
+import os
 
 #아래 4줄은 openai GPT 호출과 환경변수 로딩을 가능하게 하기 위한 부분
-#import openai
-#from dotenv import load_dotenv
-#load_dotenv()
-#openai.api_key = os.getenv("OPENAI_API_KEY")
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 
@@ -27,7 +27,7 @@ async def analyze(request: AnalyzeRequest):
     return {"sentence": sentence, "diagramming": diagram}
 
 #아래는 함수는 FastAPI에서 /openapi.json 엔드포인트 추가하기 위한 부분
-#@app.get("/custom-openapi.json", include_in_schema=False)
-#async def serve_openapi():
-#    file_path = os.path.join(os.path.dirname(__file__), "..", "openapi.json")
-#    return FileResponse(file_path, media_type="application/json")
+@app.get("/custom-openapi.json", include_in_schema=False)
+async def serve_openapi():
+    file_path = os.path.join(os.path.dirname(__file__), "..", "openapi.json")
+    return FileResponse(file_path, media_type="application/json")
