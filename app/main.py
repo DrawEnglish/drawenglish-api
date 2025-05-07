8d656b6a5be00276429ab4989b31fce0ee7cb7a7
DrawEnglihs <drawenglish@gmail.com>
2025-05-08 01:07:22 +0900

    fix: add uvicorn run block

--- app/main.py
+++ app/main.py
@@ -284,4 +284,4 @@ async def analyze(request: AnalyzeRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
